dd24486a179822294cf953f210810a716398b9e3
Zack Campbell <zajrik@users.noreply.github.com>
2023-06-08 14:29:49 -0500

    re-order tests, add more tests

--- test/option_result_test.dart
+++ test/option_result_test.dart
@@ -2,6 +2,63 @@ import 'package:option_result/option_result.dart';
 import 'package:test/test.dart';
 
 void main() {
+	group('Option:', () {
+		test('Should return appropriate values for isSome()/isNone()', () {
+			expect(Some(null).isSome(), equals(true));
+			expect(Some(null).isNone(), equals(false));
+			expect(None().isSome(), equals(false));
+			expect(None().isNone(), equals(true));
+		});
+
+		test('Should hold and unwrap simple values', () {
+			expect(Some('foo bar baz').unwrap(), equals('foo bar baz'));
+			expect(Some(42).unwrap(), equals(42));
+			expect(Some(false).unwrap(), equals(false));
+		});
+
+		test('Should hold and unwrap complex values', () {
+			expect(Some({'foo': 'bar', 'baz': 42}).unwrap(), equals({'foo': 'bar', 'baz': 42}));
+			expect(Some(['foo', 'bar', 'baz']).unwrap(), equals(['foo', 'bar', 'baz']));
+		});
+
+		test('Should return appropriate values from unwrapOr()', () {
+			expect(Some(1).unwrapOr(2), equals(1));
+			expect(None().unwrapOr(2), equals(2));
+		});
+
+		test('Should equate equatable Options', () {
+			expect(Some('foo') == Some('foo'), equals(true));
+			expect(None() == None(), equals(true));
+
+			Map<String, dynamic> foo = {'foo': 'bar', 'baz': 42};
+
+			// They share the same reference to foo
+			expect(Some(foo) == Some(foo), equals(true));
+		});
+
+		test('Should create appropriate Options via Option.from()', () {
+			expect(Option.from('foo'), equals(Some('foo')));
+			expect(Option<int>.from(null), equals(None<int>()));
+		});
+
+		test('Should not equate Options with equatable values but mismatched types', () {
+			Option<int> foo = Some(1);
+			Option<num> bar = Some(1);
+
+			expect(foo == bar, equals(false));
+
+			Option<int> foo2 = None();
+			Option<bool> bar2 = None();
+
+			// ignore: unrelated_type_equality_checks
+			expect(foo2 == bar2, equals(false));
+		});
+
+		test('Should throw OptionError when unwrapping None()', () {
+			expect(() => None().unwrap(), throwsA(TypeMatcher<OptionError>()));
+		});
+	});
+
 	group('Result:', () {
 		test('Should return appropriate values for isOk()/isErr()', () {
 			expect(Ok(null).isOk(), equals(true));
@@ -10,24 +67,24 @@ void main() {
 			expect(Err(null).isErr(), equals(true));
 		});
 
-		test('Should hold a simple Ok value', () {
+		test('Should hold and unwrap simple Ok values', () {
 			expect(Ok('foo bar baz').unwrap(), equals('foo bar baz'));
 			expect(Ok(42).unwrap(), equals(42));
 			expect(Ok(false).unwrap(), equals(false));
 		});
 
-		test('Should hold a simple Err value', () {
+		test('Should hold and unwrap simple Err values (unwrapErr)', () {
 			expect(Err('foo bar baz').unwrapErr(), equals('foo bar baz'));
 			expect(Err(42).unwrapErr(), equals(42));
 			expect(Err(false).unwrapErr(), equals(false));
 		});
 
-		test('Should hold a complex Ok value', () {
+		test('Should hold and unwrap complex Ok values', () {
 			expect(Ok({'foo': 'bar', 'baz': 42}).unwrap(), equals({'foo': 'bar', 'baz': 42}));
 			expect(Ok(['foo', 42, true]).unwrap(), equals(['foo', 42, true]));
 		});
 
-		test('Should hold a complex Err value', () {
+		test('Should hold and unwrap complex Err values (unwrapErr)', () {
 			expect(Err({'foo': 'bar', 'baz': 42}).unwrapErr(), equals({'foo': 'bar', 'baz': 42}));
 			expect(Err(['foo', 42, true]).unwrapErr(), equals(['foo', 42, true]));
 		});
@@ -76,6 +133,42 @@ void main() {
 			expect(() => Err('foo bar baz').unwrap(), throwsA(TypeMatcher<ResultError>()));
 		});
 
+		test('Should throw ResultError with unwrapErr() on Ok()', () {
+			expect(() => Ok('foo bar baz').unwrapErr(), throwsA(TypeMatcher<ResultError>()));
+		});
+	});
+
+	group('Helpers:', () {
+		// Option helpers
+		test('Should propagate None() via propagateOption', () {
+			expect(propagateOption<int>(() {
+				Option<int> foo = Some(1);
+				Option<int> bar = None();
+				return Some(foo.unwrap() + bar.unwrap());
+			}), equals(None<int>()));
+		});
+
+		test('Should propagate None() via propagateOptionAsync', () async {
+			expect(await propagateOptionAsync<int>(() {
+				Option<int> foo = Some(1);
+				Option<int> bar = None();
+				return Some(foo.unwrap() + bar.unwrap());
+			}), equals(None<int>()));
+		});
+
+		test('Should rethrow any other kind of error/exception thrown inside propagateOption', () {
+			expect(() => propagateOption(() => throw RangeError('foo')), throwsRangeError);
+			expect(() => propagateOption(() => throw ArgumentError('bar')), throwsArgumentError);
+			expect(() => propagateOption(() => throw FormatException('baz')), throwsFormatException);
+		});
+
+		test('Should rethrow any other kind of error/exception thrown inside propagateOptionAsync', () {
+			expect(() => propagateOptionAsync(() => throw RangeError('foo')), throwsRangeError);
+			expect(() => propagateOptionAsync(() => throw ArgumentError('bar')), throwsArgumentError);
+			expect(() => propagateOptionAsync(() => throw FormatException('baz')), throwsFormatException);
+		});
+
+		// Result helpers
 		test('Should propagate Err() values via propagateResult', () {
 			expect(propagateResult<int, String>(() {
 				Result<int, String> foo = Ok(1);
@@ -84,16 +177,22 @@ void main() {
 			}), equals(Err<int, String>('foo bar baz')));
 		});
 
-		test('Should throw ResultError when propagating a mismatched Err() type via propagateResult', () {
-			expect(() => propagateResult(() {
+		test('Should propagate Err() values via propagateResultAsync', () async {
+			expect(await propagateResultAsync<int, String>(() {
 				Result<int, String> foo = Ok(1);
-				Result<bool, String> bar = Err('foo bar baz');
-				return Ok(foo.unwrap() + (bar.unwrap() ? 1 : 2));
-			}), throwsA(TypeMatcher<ResultError>()));
+				Result<int, String> bar = Err('foo bar baz');
+				return Ok(foo.unwrap() + bar.unwrap());
+			}), equals(Err<int, String>('foo bar baz')));
 		});
 
 		test('Should rethrow ResultError when erroring on unwrapErr() on Ok() via propagateResult', () {
 			expect(() => propagateResult<int, String>(() {
+				return Ok(Ok(1).unwrapErr());
+			}), throwsA(TypeMatcher<ResultError>()));
+		});
+
+		test('Should rethrow ResultError when erroring on unwrapErr() on Ok() via propagateResultAsync', () {
+			expect(() => propagateResultAsync<int, String>(() {
 				return Ok(Ok(1).unwrapErr());
 			}), throwsA(TypeMatcher<ResultError>()));
 		});
@@ -110,12 +209,12 @@ void main() {
 			expect(() => propagateResultAsync(() => throw FormatException('baz')), throwsFormatException);
 		});
 
-		test('Should propagate Err() values via propagateResultAsync', () async {
-			expect(await propagateResultAsync<int, String>(() {
+		test('Should throw ResultError when propagating a mismatched Err() type via propagateResult', () {
+			expect(() => propagateResult<int, String>(() {
 				Result<int, String> foo = Ok(1);
-				Result<int, String> bar = Err('foo bar baz');
-				return Ok(foo.unwrap() + bar.unwrap());
-			}), equals(Err<int, String>('foo bar baz')));
+				Result<bool, String> bar = Err('foo bar baz');
+				return Ok(foo.unwrap() + (bar.unwrap() ? 1 : 2));
+			}), throwsA(TypeMatcher<ResultError>()));
 		});
 
 		test('Should throw ResultError when propagating a mismatched Err() type via propagateResultAsync', () {
@@ -124,94 +223,6 @@ void main() {
 				Result<bool, String> bar = Err('foo bar baz');
 				return Ok(foo.unwrap() + (bar.unwrap() ? 1 : 2));
 			}), throwsA(TypeMatcher<ResultError>()));
-		});
-
-		test('Should rethrow ResultError when erroring on unwrapErr() on Ok() via propagateResultAsync', () {
-			expect(() => propagateResultAsync<int, String>(() {
-				return Ok(Ok(1).unwrapErr());
-			}), throwsA(TypeMatcher<ResultError>()));
-		});
-	});
-
-	group('Option:', () {
-		test('Should return appropriate values for isSome()/isNone()', () {
-			expect(Some(null).isSome(), equals(true));
-			expect(Some(null).isNone(), equals(false));
-			expect(None().isSome(), equals(false));
-			expect(None().isNone(), equals(true));
-		});
-
-		test('Should hold a simple value', () {
-			expect(Some('foo bar baz').unwrap(), equals('foo bar baz'));
-			expect(Some(42).unwrap(), equals(42));
-			expect(Some(false).unwrap(), equals(false));
-		});
-
-		test('Should hold a complex value', () {
-			expect(Some({'foo': 'bar', 'baz': 42}).unwrap(), equals({'foo': 'bar', 'baz': 42}));
-		});
-
-		test('Should return appropriate value from unwrapOr()', () {
-			expect(Some(1).unwrapOr(2), equals(1));
-			expect(None().unwrapOr(2), equals(2));
-		});
-
-		test('Should equate equatable Options', () {
-			expect(Some('foo') == Some('foo'), equals(true));
-			expect(None() == None(), equals(true));
-
-			Map<String, dynamic> foo = {'foo': 'bar', 'baz': 42};
-			expect(Some(foo) == Some(foo), equals(true));
-		});
-
-		test('Should create appropriate Options via Option.from()', () {
-			expect(Option.from('foo'), equals(Some('foo')));
-			expect(Option<int>.from(null), equals(None<int>()));
-		});
-
-		test('Should not equate Options with equatable values but mismatched types', () {
-			Option<int> foo = Some(1);
-			Option<num> bar = Some(1);
-
-			expect(foo == bar, equals(false));
-
-			Option<int> foo2 = None();
-			Option<bool> bar2 = None();
-
-			// ignore: unrelated_type_equality_checks
-			expect(foo2 == bar2, equals(false));
-		});
-
-		test('Should throw OptionError when unwrapping None()', () {
-			expect(() => None().unwrap(), throwsA(TypeMatcher<OptionError>()));
-		});
-
-		test('Should rethrow any other kind of error/exception thrown inside propagateOption', () {
-			expect(() => propagateOption(() => throw RangeError('foo')), throwsRangeError);
-			expect(() => propagateOption(() => throw ArgumentError('bar')), throwsArgumentError);
-			expect(() => propagateOption(() => throw FormatException('baz')), throwsFormatException);
-		});
-
-		test('Should rethrow any other kind of error/exception thrown inside propagateOptionAsync', () {
-			expect(() => propagateOptionAsync(() => throw RangeError('foo')), throwsRangeError);
-			expect(() => propagateOptionAsync(() => throw ArgumentError('bar')), throwsArgumentError);
-			expect(() => propagateOptionAsync(() => throw FormatException('baz')), throwsFormatException);
-		});
-
-		test('Should propagate None() via propagateOption', () {
-			expect(propagateOption<int>(() {
-				Option<int> foo = Some(1);
-				Option<int> bar = None();
-				return Some(foo.unwrap() + bar.unwrap());
-			}), equals(None<int>()));
-		});
-
-		test('Should propagate None() via propagateOptionAsync', () async {
-			expect(await propagateOptionAsync<int>(() {
-				Option<int> foo = Some(1);
-				Option<int> bar = None();
-				return Some(foo.unwrap() + bar.unwrap());
-			}), equals(None<int>()));
 		});
 	});
 }
